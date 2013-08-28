46faaefbb1171498032c05db90a8db0353f9edec
Kyle Kochis <kkochis@returnpath.net>
2013-08-28 08:55:41 -0600

    Removed horrible $http.get nesting and refactored into $watches
    Change-Id: Ia44f41872b1288a7115c6ae3e68b4a7692819808

--- js/controllers.js
+++ js/controllers.js
@@ -8,7 +8,6 @@ angular.module('dashES.controllers',[])
 
       $scope.app_config = appConfig;
       $scope.app_config.es_host = 'http://localhost:9200';
-      $scope.cluster = dashboardData.get_health();
 
   }])
 
@@ -19,6 +18,13 @@ angular.module('dashES.controllers',[])
 
       $scope.app_config = appConfig;
       $scope.cluster = dashboardData.get_health();
+      $scope.index_stats = dashboardData.get_indices();
+      // .routing_nodes.nodes
+      // .routing_table.indices
+      $scope.routing = dashboardData.get_routing();
+      $scope.node_stats = dashboardData.get_node_stats();
+      $scope.indices = [];
+      $scope.nodes = [];
 
       $scope.$watch('cluster', function(cluster) {
         if (angular.isDefined(cluster)) {
@@ -58,64 +64,62 @@ angular.module('dashES.controllers',[])
             cluster.unassigned_shards_label = "success";
           }
         }
-        else {
-          
-        }
       });
 
-
-      // I need to split out these into services still:
-      $http.get('http://localhost:9200/_stats?clear=true&docs=true&store=true&indexing=true').success(function(data) {
-        $scope.indices = data.indices;
-
-        $http.get('http://localhost:9200/_cluster/state?filter_indices=true&filter_blocks=true&filter_metadata=true&filter_nodes=true').success(function(data) {
-          $scope.routing_nodes = data.routing_nodes.nodes;
-          // Turning the Index info into an array so Angualar can sort it
-          var indices = $.map(data.routing_table.indices, function(k, v) {
+      $scope.$watch('[routing.routing_table, index_stats]', function (args) {
+        // Turning the Index info into an array so Angualar can sort it and joining in some of the
+        // index routing data
+        var routing_table = args[0];
+        var index_stats = args[1];
+        if (angular.isDefined(routing_table) && angular.isDefined(index_stats)) {
+          var indices = $.map(routing_table.indices, function(k, v) {
             k.key = v;
-            k.stats = $scope.indices[v]
+            k.stats = index_stats.indices[v]
             return [k];
           });
           $scope.indices = indices;
+        }
+      }, true);
 
-          $http.get('http://localhost:9200/_cluster/nodes/stats').success(function(data) {
-            // Turning the object into an array so Angular can sort it
-            var nodes = $.map(data.nodes, function(k, v) {
-                k.key = v;
-                k.shard_count = 0;
-                k.started_shard_count = 0;
-                k.relocating_shard_count = 0;
-                k.initializing_shard_count = 0;
-                k.bad_initializing_shard_count = 0;
+      $scope.$watch('[routing.routing_nodes.nodes, node_stats.nodes]', function (args) {
+        // Turning the object into an array so Angular can sort it
+        var routing_nodes = args[0];
+        var node_stats = args[1];
+        if (angular.isDefined(node_stats) && angular.isDefined(routing_nodes)) {
+          var nodes = $.map(node_stats, function(k, v) {
+              k.key = v;
+              k.shard_count = 0;
+              k.started_shard_count = 0;
+              k.relocating_shard_count = 0;
+              k.initializing_shard_count = 0;
+              k.bad_initializing_shard_count = 0;
 
-                // Getting the number of shards on each node
-                if ($scope.routing_nodes[v]) {
-                  k.shard_count = $scope.routing_nodes[v].length;
-                  k.started_shard_count = $filter('filter')($scope.routing_nodes[v], {"state" : "STARTED"}).length;
-                  k.relocating_shard_count = $filter('filter')($scope.routing_nodes[v], {"state" : "RELOCATING"}).length;
-                  k.initializing_shard_count = $filter('filter')($scope.routing_nodes[v], {"state" : "INITIALIZING"}).length;
+              // Getting the number of shards on each node
+              if (routing_nodes[v]) {
+                k.shard_count = routing_nodes[v].length;
+                k.started_shard_count = $filter('filter')(routing_nodes[v], {"state" : "STARTED"}).length;
+                k.relocating_shard_count = $filter('filter')(routing_nodes[v], {"state" : "RELOCATING"}).length;
+                k.initializing_shard_count = $filter('filter')(routing_nodes[v], {"state" : "INITIALIZING"}).length;
 
-                  // Why not just use the filtering with an object? Because there seemse to be a bug
-                  // with null properties and the filtering so I have to do it the ol' fashioned way
-                  for (var i = $filter('filter')($scope.routing_nodes[v], {"state" : "INITIALIZING"}).length - 1; i >= 0; i--){
-                    if ($filter('filter')($scope.routing_nodes[v], {"state" : "INITIALIZING"})[i].relocating_node == null) {
-                      k.bad_initializing_shard_count++
-                    }
+                // Why not just use the filtering with an object? Because there seemse to be a bug
+                // with null properties and the filtering so I have to do it the ol' fashioned way
+                for (var i = $filter('filter')(routing_nodes[v], {"state" : "INITIALIZING"}).length - 1; i >= 0; i--){
+                  if ($filter('filter')(routing_nodes[v], {"state" : "INITIALIZING"})[i].relocating_node == null) {
+                    k.bad_initializing_shard_count++
                   }
                 }
+              }
 
-                if (k.bad_initializing_shard_count > 0) {
-                  k.initializing_class = 'important';
-                } else if (k.initializing_shard_count > 0) {
-                  k.initializing_class = 'warning';
-                }
+              if (k.bad_initializing_shard_count > 0) {
+                k.initializing_class = 'important';
+              } else if (k.initializing_shard_count > 0) {
+                k.initializing_class = 'warning';
+              }
 
-                return [k];
-            });
-            $scope.nodes = nodes;
+              return [k];
           });
+          $scope.nodes = nodes;
+        }
+      }, true);
 
-        });
-
-      });
   }]);
